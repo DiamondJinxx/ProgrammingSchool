e6e858da72bbf3b041408932cedc951322579626
Smotrov Dmitriy <jinxx_develop@mail.ru>
2023-03-29 20:59:32 +0300

    insert with error

--- AbstractDataStructure/DynamicArray/dynamic_array.py
+++ AbstractDataStructure/DynamicArray/dynamic_array.py
@@ -1,35 +1,56 @@
 import ctypes
+
+
 decrease = 1.5
-increase =2
+increase = 2
+
 
 class DynArray:
-    _min_capacity = 16
-
+    __min_capacity = 16
+    
     def __init__(self):
         self.count = 0
-        self.capacity = DynArray._min_capacity
+        self.capacity = DynArray.__min_capacity
         self.array = self.make_array(self.capacity)
 
     def __len__(self):
         return self.count
-    
+
     def make_array(self, new_capacity):
         return (new_capacity * ctypes.py_object)()
-    
-    def __getitem__(self, i):
+
+    def __getitem__(self,i):
         if i < 0 or i >= self.count:
             raise IndexError('Index is out of bounds')
         return self.array[i]
-    
+
     def resize(self, new_capacity):
         new_array = self.make_array(new_capacity)
+        print('resize')
         for i in range(self.count):
             new_array[i] = self.array[i]
         self.array = new_array
         self.capacity = new_capacity
-    
+
     def append(self, itm):
         if self.count == self.capacity:
-            self.resize(increase * self.capacity)
+            self.resize(increase*self.capacity)
         self.array[self.count] = itm
         self.count += 1
+
+    def insert(self, i, itm):
+        if i < 0 or i > self.count:
+            raise IndexError('Index is out of bounds')
+        if i == self.count:
+            self.append(itm)
+            return
+        if self.count + 1 >= self.capacity:
+            self.resize(increase*self.capacity)
+        for j in range(self.count, i - 1, -1):
+            self.array[j] = self.array[j - 1]
+        self.array[i] = itm
+        self.count += 1
+        
+
+    def delete(self, i):
+        pass
