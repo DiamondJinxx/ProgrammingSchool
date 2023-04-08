c1fcf277fee5d6b458cf05d150641f01bd6201ab
Smotrov Dmitriy <jinxx_develop@mail.ru>
2023-04-08 21:33:12 +0300

    add runtime and is_balanced method

--- AbstractDataStructure/Stack/runtime.py
+++ AbstractDataStructure/Stack/runtime.py
@@ -1,17 +1,55 @@
-from stack import Stack
+from stack import Stack as StackHead
+from stack_tail import StackTail
+import time
 
 
-stack = Stack()
 
-stack.push(1)
-stack.push('2')
-stack.push(1.123)
-stack.push('some new stack element')
-print(f'len is: {stack.size()}')
+# по замерам интересное конечно 
+# stack_head = StackHead()
 
-while not stack.is_empty():
-    print(stack.pop())
+# start_time = time.time()
+# for i in range(100_000):
+#     stack_head.push(i)
+# print(f'Добавление элементов в stack_head: {time.time() - start_time}')
 
-print(stack.pop())
-print(stack.peek())
-print(f'len is: {stack.size()}')
+# stack_tail = StackTail()
+
+# start_time = time.time()
+# for i in range(100_000):
+#     stack_tail.push(i)
+# print(f'Добавление элементов в stack_tail: {time.time() - start_time}')
+
+# for i in range(100_000):
+#     stack_head.pop()
+# print(f'Удаление элементов из stack_head: {time.time() - start_time}')
+
+# stack_tail = StackTail()
+
+# start_time = time.time()
+# for i in range(100_000):
+#     stack_tail.pop()
+# print(f'Удаление элементов из stack_tail: {time.time() - start_time}')
+
+def is_balanced(line: str) -> bool:
+    open_brackets = '({['
+    close_brackets = ']})'
+    brackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    stack = StackHead()
+    for char in line:
+        if char not in open_brackets and char not in close_brackets:
+            continue
+        if char in open_brackets:
+            stack.push(char)
+            continue
+        if char in close_brackets and stack.peek() == brackets[char]:
+            stack.pop()
+            continue
+        return False
+    return stack.is_empty()
+
+line_balanced = ''
+print(is_balanced(line_balanced))
--- AbstractDataStructure/Stack/stack.py
+++ AbstractDataStructure/Stack/stack.py
@@ -10,7 +10,7 @@ class Stack:
         return value
 
     def push(self, value):
-        self.stack.insert(0,value)
+        self.stack.insert(0, value)
 
     def peek(self):
         value = None if self.is_empty() else self.stack[0]
@@ -18,3 +18,25 @@ class Stack:
 
     def is_empty(self) -> bool:
         return self.size() == 0
+
+
+def is_balanced(line: str) -> bool:
+    open_brackets = '({['
+    close_brackets = ']})'
+    brackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    stack = Stack()
+    for char in line:
+        if char not in open_brackets and char not in close_brackets:
+            continue
+        if char in open_brackets:
+            stack.push(char)
+            continue
+        if char in close_brackets and stack.peek() == brackets[char]:
+            stack.pop()
+            continue
+        return False
+    return stack.is_empty()
