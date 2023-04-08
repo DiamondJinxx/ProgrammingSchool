020bc1479c8e8bdd72fb665c39893798f9c95866
Smotrov Dmitriy <jinxx_develop@mail.ru>
2023-04-08 19:19:39 +0300

    working stack, tail, without comments

--- AbstractDataStructure/Stack/stack.py
+++ AbstractDataStructure/Stack/stack.py
@@ -6,17 +6,15 @@ class Stack:
         return len(self.stack)
 
     def pop(self):
-        # ваш код
         value = None if self.is_empty() else self.stack.pop()
         return value
 
     def push(self, value):
-        # ваш код
         self.stack.append(value)
 
     def peek(self):
         value = None if self.is_empty() else self.stack[-1]
         return value
-    
+
     def is_empty(self) -> bool:
         return self.size() == 0
