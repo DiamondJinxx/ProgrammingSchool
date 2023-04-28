6463a86945a2dcbef024095ea59a3ab74c7c2e3b
Smotrov Dmitriy <jinxx_develop@mail.ru>
2023-04-28 21:52:30 +0300

    all tests

--- AbstractDataStructure/HashTable/tests.py
+++ AbstractDataStructure/HashTable/tests.py
@@ -4,15 +4,43 @@ from hash_table import HashTable
 
 class HashTableTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.ht = HashTable(19, 3)
+
     def test_hash_fun(self):
-         pass
+        value = 'some_string'
+        expected_hash = len(value) % 19
+        self.assertEqual(expected_hash, self.ht.hash_fun(value))
     
     def test_seek_slot(self):
-         pass
+        string = 'some_string'
+        expected_slot = self.ht.hash_fun(string)
+        self.assertEqual(expected_slot, self.ht.seek_slot(string))
+
+        # collision
+        for i in range(self.ht.size):
+            self.ht.put(string)
+        self.assertIsNone(self.ht.seek_slot(string))
     
     def test_put(self):
-         pass
+        string = 'some_string'
+        expected_slot = self.ht.hash_fun(string)
+        slot = self.ht.put(string)
+        self.assertEqual(expected_slot, slot)
+
+        # collision
+        for i in range(self.ht.size):
+            self.ht.put(string)
+        self.assertIsNone(self.ht.put(string))
+
     
     def test_find(self):
-        pass
+        value = 'some_string'
+        index = self.ht.hash_fun(value)
+        self.ht.put(value)
+        self.assertEqual(index, self.ht.find(value))
+
+        not_in_table = 'not_in_table'
+        self.assertIsNone(self.ht.find(not_in_table))
+
     
