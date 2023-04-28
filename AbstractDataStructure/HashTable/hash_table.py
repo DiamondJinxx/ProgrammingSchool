722e8a43505b30306128ca7727bf6e829f37a9fb
Smotrov Dmitriy <jinxx_develop@mail.ru>
2023-04-28 21:42:29 +0300

    seek_slot, find

--- AbstractDataStructure/HashTable/hash_table.py
+++ AbstractDataStructure/HashTable/hash_table.py
@@ -5,28 +5,32 @@ class HashTable:
         self.slots = [None] * self.size
 
     def hash_fun(self, value):
-        # в качестве value поступают строки!
         return len(value) % self.size
 
     def seek_slot(self, value):
-        # находит индекс пустого слота для значения, или None
+        slot_index = self.hash_fun(value)
+        if self.slots[slot_index] is None:
+            return slot_index
+        # while don't come back in index from hash_func - run
+        i = 0
+        while i < self.size:
+            slot_index = (slot_index + self.step) % self.size
+            if self.slots[slot_index] is None:
+                return slot_index
+            i += 1
         return None
 
     def put(self, value):
-        # записываем значение по хэш-функции
-        
-        # возвращается индекс слота или None,
-        # если из-за коллизий элемент не удаётся
-        # разместить 
         slot = self.seek_slot(value)
         if slot is not None:
             self.slots[slot] = value
         return slot
 
     def find(self, value):
-        # находит индекс слота со значением, или None
         slot = self.hash_fun(value)
         if self.slots[slot] == value:
             return slot
-
-        return None
+        try:
+            return self.slots.index(value)
+        except ValueError:
+            return None
