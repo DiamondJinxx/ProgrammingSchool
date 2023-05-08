43f6fedda040064ef3c6876f6d7c8c90da293182
Smotrov Dmitriy <jinxx_develop@mail.ru>
2023-05-08 21:00:17 +0300

    tests for remove, put

--- AbstractDataStructure/Set/tests.py
+++ AbstractDataStructure/Set/tests.py
@@ -3,19 +3,33 @@ from powerset import PowerSet
 
 class TestPowerSet(unittest.TestCase):
     def setUp(self):
-        pass
+        self.ps = PowerSet()
 
     def test_put(self):
-        pass
-
-    def test_size(self):
-        pass
+        element = 1
+        self.ps.put(element)
+        self.assertEqual(self.ps.size(), 1)
+        self.assertTrue(self.ps.data[0] is element)
+    
+    def test_put_same_elements(self):
+        element = 12
+        self.ps.put(element)
+        self.assertEqual(self.ps.size(), 1)
+        self.ps.put(element)
+        self.assertEqual(self.ps.size(), 1)
 
     def test_get(self):
         pass
 
     def test_remove(self):
-        pass
+        start_size = 10
+        remove_item = 5
+        for i in range(start_size):
+            self.ps.put(i)
+        self.assertEqual(self.ps.size(), start_size)
+        self.ps.remove(remove_item)
+        self.assertEqual(self.ps.size(), start_size - 1)
+        self.assertTrue(not self.ps.get(remove_item))
 
     def test_intersection(self):
         pass
