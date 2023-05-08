c98f6479f6982df47e906f92ee18ee5d5f583087
Smotrov Dmitriy <jinxx_develop@mail.ru>
2023-05-08 22:29:22 +0300

    full tests

--- AbstractDataStructure/Set/tests.py
+++ AbstractDataStructure/Set/tests.py
@@ -18,9 +18,6 @@ class TestPowerSet(unittest.TestCase):
         self.ps.put(element)
         self.assertEqual(self.ps.size(), 1)
 
-    def test_get(self):
-        pass
-
     def test_remove(self):
         start_size = 10
         remove_item = 5
@@ -32,13 +29,91 @@ class TestPowerSet(unittest.TestCase):
         self.assertTrue(not self.ps.get(remove_item))
 
     def test_intersection(self):
-        pass
+        expected_size = 2
+        expected_values = [5, 6]
+        for i in range(10):
+            self.ps.put(i)
+        set2 = PowerSet()
+        set2_values = expected_values + [10, 12]
+        for value in set2_values:
+            set2.put(value)
+        inter = self.ps.intersection(set2)
+        self.assertEqual(inter.size(), expected_size)
+        for value in expected_values:
+            self.assertTrue(inter.get(value))
+
+    def test_empty_intersection(self):
+        for i in range(10):
+            self.ps.put(i)
+        set2 = PowerSet()
+        set2_values = [10, 12]
+        for value in set2_values:
+            set2.put(value)
+        inter = self.ps.intersection(set2)
+        self.assertEqual(inter.size(), 0 )
 
     def test_union(self):
-        pass
+        expected_size = 12
+        expected_values = [i for i in range(10)] + [i for i in range(10, 12)]
+        for i in range(10):
+            self.ps.put(i)
+        set2 = PowerSet()
+        for i in range(5, 12):
+            set2.put(i)
+
+        union = self.ps.union(set2)
+        self.assertEqual(union.size(), expected_size)
+        for val in expected_values:
+            self.assertTrue(union.get(val))
+
+    def test_union_empty_second_param(self):
+        expected_size = 10
+        expected_values = [i for i in range(10)] 
+        for i in range(10):
+            self.ps.put(i)
+        set2 = PowerSet()
+
+        union = self.ps.union(set2)
+        self.assertEqual(union.size(), expected_size)
+        for val in expected_values:
+            self.assertTrue(union.get(val))
 
     def test_difference(self):
-        pass
+        expected_size = 5
+        for i in range(10):
+            self.ps.put(i)
+        set2 = PowerSet()
+        for i in range(5):
+            set2.put(i)
+        diff = self.ps.difference(set2)
+        self.assertEqual(diff.size(), expected_size)
+        for i in range(5, 10):
+            self.assertTrue(diff.get(i)) 
+        
+
+    def test_difference_empty_result(self):
+        set2 = PowerSet()
+        for i in range(10):
+            self.ps.put(i)
+            set2.put(i)
+        
+        diff = self.ps.difference(set2)
+        self.assertEqual(diff.size(), 0)
 
     def test_issubset(self):
-        pass
+        set2 = PowerSet()
+        set3 = PowerSet()
+        set4 = PowerSet()
+        for i in range(10):
+            self.ps.put(i)
+            set2.put(i)
+        for i in range(20):
+            set3.put(i) 
+        for i in range(5):
+            set4.put(i)
+        set4.put(14)
+        set4.put(15)
+        self.assertTrue(self.ps.issubset(set2))
+        self.assertFalse(self.ps.issubset(set3))
+        self.assertFalse(self.ps.issubset(set4))
+
