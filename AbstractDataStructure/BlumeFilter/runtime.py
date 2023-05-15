f9cec772bbcdfda5b65c6e70566d3e9c0a962b97
Dmitriy Smotrov <jinxx_develop@mail.ru>
2023-05-15 09:36:08 +0300

    tests in runtime

--- AbstractDataStructure/BlumeFilter/runtime.py
+++ AbstractDataStructure/BlumeFilter/runtime.py
@@ -4,22 +4,15 @@ def bin_print(num: int):
     print('{0:b}'.format(num))
 
 bf = BloomFilter(32)
-arr = int()
-# print(type(arr))
-# # set to one
-# # print(arr | 1 << 7)
-# # set to zero
-# # print(arr & ~(1 << 7))
-# v = arr | 1 << 5
-# bin_print(10)
-# print(v)
-# #read 
-# print(v & 1 << 5)
-# v = arr & ~(1 << 5)
-# print(v)
 test = '0123456789'
-for i in range(10):
-    print(test[i:] + test[: i])
+test_keys = [test[i:] + test[: i] for i in range(10)]
+i = 0
+for key in test_keys:
+    i += 1
+    if i % 2 == 0:
+        continue
+    bf.add(key)
+    print(i)
 
-# print(bf.hash1(test))
-# print(bf.hash2(test))
+for key in test_keys:
+    print(key, bf.is_value(key))
