29499b0f15dcac2b5adaaad2fdd17c9d3de4dcf2
Dmitriy Smotrov <jinxx_develop@mail.ru>
2023-09-09 17:59:23 +0300

    move outside dfs method

--- AbstractDataStructure/ArrayBinaryTree/ArrayBinaryTree.py
+++ AbstractDataStructure/ArrayBinaryTree/ArrayBinaryTree.py
@@ -4,24 +4,10 @@ class aBST:
         tree_size = 2**(depth + 1) - 1
         self.depth = depth
         self.Tree = [None] * tree_size # массив ключей
-	
+
     def FindKeyIndex(self, key):
         # ищем в массиве индекс ключа
-        def dfs(root_idx, key):
-            if root_idx >= len(self):
-                return None
-            if self.Tree[root_idx] == key:
-                return root_idx
-            if self.Tree[root_idx] is None:
-                return -root_idx
-            left_child = root_idx * 2 + 1
-            right_child = root_idx * 2 + 2
-            if key < self.Tree[root_idx]:
-                return dfs(left_child, key)
-            if key > self.Tree[root_idx]:
-                return dfs(right_child, key)
-            
-        return dfs(0, key)
+        return self.dfs(0, key)
 	
     def AddKey(self, key):
         # добавляем ключ в массив
@@ -34,6 +20,20 @@ class aBST:
             return index_for_new_node
         self.Tree[-index_for_new_node] = key
         return -index_for_new_node
+
+    def dfs(self, root_idx, key):
+        if root_idx >= len(self):
+            return None
+        if self.Tree[root_idx] == key:
+            return root_idx
+        if self.Tree[root_idx] is None:
+            return -root_idx
+        left_child = root_idx * 2 + 1
+        right_child = root_idx * 2 + 2
+        if key < self.Tree[root_idx]:
+            return self.dfs(left_child, key)
+        if key > self.Tree[root_idx]:
+            return self.dfs(right_child, key)
 
     def __len__(self):
         return len(self.Tree)
