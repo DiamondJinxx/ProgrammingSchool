c9d64c5c966f6c09dfc1c7690419b0be08e2ea5c
Dmitriy Smotrov <dsmotrov@astralinux.ru>
2024-01-31 01:29:12 +0300

    task_2: vehicle brand models and relations one to many

--- AutoPark/src/auto/admin.py
+++ AutoPark/src/auto/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from auto.models import Vehicle
+from auto.models import Vehicle, VehicleType, Brand
 
 # Register your models here.
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'price', 'mileage', 'release_year']
+    list_display = ['id', 'price', 'mileage', 'release_year', 'brand']
 
+
+@admin.register(VehicleType)
+class VehicleTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description']
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'load_capacity', 'number_of_seats', 'fuel_capacity', 'max_speed', 'vehicle_type']
--- AutoPark/src/auto/models.py
+++ AutoPark/src/auto/models.py
@@ -6,8 +6,35 @@ class Vehicle(models.Model):
     price = models.IntegerField()
     mileage = models.IntegerField()
     release_year = models.IntegerField()
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "Транспортное средство"
         verbose_name_plural = "Транспортные средства"
 
+
+class VehicleType(models.Model):
+    description = models.CharField(max_length=40)
+
+    class Meta:
+        verbose_name = "Тип транспортного средства"
+        verbose_name_plural = "Типы транспортных средст"
+
+    def __str__(self):
+        return self.description
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=40)
+    load_capacity = models.DecimalField(max_digits=4, decimal_places=2)
+    number_of_seats = models.IntegerField() 
+    fuel_capacity = models.IntegerField()
+    max_speed = models.IntegerField()
+    vehicle_type = models.ForeignKey(VehicleType, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = "Автомобильный бренд"
+        verbose_name_plural = "Автомобильные бренды"
+
+    def __str__(self):
+        return f"{self.name}: {self.vehicle_type.description}"
