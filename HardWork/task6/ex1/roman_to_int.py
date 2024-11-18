e4836c28f6f053a1b2e731961647df7a20e07ff5
Dmitriy Smotrov <dsmotrov@astralinux.ru>
2024-11-18 20:49:53 +0300

    HW6: ex1, add complex sub test

--- HardWork/task6/ex1/roman_to_int.py
+++ HardWork/task6/ex1/roman_to_int.py
@@ -1,15 +1,24 @@
+
 def romanToInt(roman: str) -> int:
-    if roman == "IV":
-        return 4
     roman_to_int_map = {
         "I": 1,
         "V": 5,
         "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
     }
     roman = roman[::-1]
     integer = 0
+    prev = -1
     for number in roman:
         int_number = roman_to_int_map[number]
+        if int_number < prev:
+            integer -= int_number
+            prev = int_number
+            continue
         integer += int_number
+        prev = int_number
 
     return integer
--- HardWork/task6/ex1/test.py
+++ HardWork/task6/ex1/test.py
@@ -26,3 +26,13 @@ def test_sum() -> None:
         result = romanToInt(origin)
         assert result == expected_result
 
+def test_sub() -> None:
+    origins_with_expected_results = {
+        "XIV": 14,
+        "IX": 9,
+        "XXIX": 29,
+        "LIV": 54,
+    }
+    for origin, expected_result in origins_with_expected_results.items():
+        result = romanToInt(origin)
+        assert result == expected_result
