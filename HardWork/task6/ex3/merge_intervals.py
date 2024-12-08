ff0a8c9cf7093d0c7beae7f0e7d2b6cd4d776408
Dmitriy Smotrov <dsmotrov@astralinux.ru>
2024-12-08 22:56:29 +0300

    second test

--- HardWork/task6/ex3/merge_intervals.py
+++ HardWork/task6/ex3/merge_intervals.py
@@ -1,3 +1,10 @@
 
-def merge(intervals: list[list[int]]) -> None:
-    return [[1,5]]
+def merge(intervals: list[list[int]]) -> list[list[int]]:
+    interval1 = intervals[0]
+    interval2 = intervals[1]
+    result = [interval1]
+    if interval2[0] <= interval1[1]:
+        result[-1][1] = interval2[1]
+    else:
+        result.append(interval2)
+    return result
--- HardWork/task6/ex3/test.py
+++ HardWork/task6/ex3/test.py
@@ -4,3 +4,9 @@ def test_easy_merging() -> None:
     data = [[1,3], [2,5]]
     result = merge(data)
     assert result == [[1,5]]
+
+def test_when_intervals_dont_have_intesaction() -> None:
+    data = [[1,3], [4,5]]
+    result = merge(data)
+    assert result == [[1,3], [4,5]]
+
